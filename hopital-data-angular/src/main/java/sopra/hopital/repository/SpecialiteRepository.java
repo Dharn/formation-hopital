e8db267cd797f879ecfc717146febac41b411f63
frankybo57 <32872685+frankybo57@users.noreply.github.com>
2017-11-28 09:46:51 +0100

    Ajout MedecinRepository & SpecialiteRepository

---
+++ hopital-data-angular/src/main/java/sopra/hopital/repository/SpecialiteRepository.java
@@ -0,0 +1,9 @@
+package sopra.hopital.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import sopra.hopital.model.Specialite;
+
+public interface SpecialiteRepository extends JpaRepository<Specialite, Integer> {
+
+}
