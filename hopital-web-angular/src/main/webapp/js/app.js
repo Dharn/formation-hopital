1d13f707883d70ef74a693b24de221334d298eed
ADEVOTI <djalexisdevoti@live.fr>
2017-11-28 09:04:45 +0100

    modif app et conexion.js

---
+++ hopital-web-angular/src/main/webapp/js/app.js
@@ -0,0 +1,16 @@
+(function() {
+	var app = angular.module("hopitalApp", [ "connexion" ]);
+
+//	app.controller("SwitchController", function() {
+//		this.onglet = "module";
+//
+//		this.setOnglet = function(val) {
+//			this.onglet = val;
+//		};
+//
+//		this.isOnglet = function(val) {
+//			return this.onglet === val;
+//		}
+//	});
+
+})();
