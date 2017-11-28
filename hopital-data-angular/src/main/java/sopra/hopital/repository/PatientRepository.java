585d7344491769354f94c69c00d3ccf6f506cf34
gaetanlzs <34029174+gaetanlzs@users.noreply.github.com>
2017-11-28 11:57:25 +0100

    Update PatientRepo

--- hopital-data-angular/src/main/java/sopra/hopital/repository/PatientRepository.java
+++ hopital-data-angular/src/main/java/sopra/hopital/repository/PatientRepository.java
@@ -1,5 +1,7 @@
 package sopra.hopital.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,5 +10,7 @@ import sopra.hopital.model.Patient;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
+	@Query("from Patient p where p.nom = :nom")
+	List<Patient> findAllByNom(String nom);
 
 }
