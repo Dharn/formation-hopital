80d2a607f09e78d678826dddecb40d58af9b9ff2
marion3133 <34029781+marion3133@users.noreply.github.com>
2017-11-28 10:23:18 +0100

    patient js salle js et index html

--- hopital-web-angular/src/main/webapp/js/salle.js
+++ hopital-web-angular/src/main/webapp/js/salle.js
@@ -5,7 +5,87 @@
 		return {
 			restrict : 'E',
 			templateUrl : "salle.html"
-			
-			};
-		});
+			controller : function($http){
+				var self = this;
+				self.salles=[];
+				self.salle=null;
+					
+				self.list = function(){
+					$http({
+						method : 'GET', 
+						url : 'api/salles'
+							
+					}).then(function success(response){
+						self.salles = response.data;
+					}, function error(response){
+							
+					});
+						
+				};
+					
+				self.add = function(){
+					self.salleForm.$setPristine();
+					self.salle = {};
+				};
+				
+				self.edit = function(){
+					self.salleForm.$setPristine();
+					$http({
+						method : 'GET',			
+						url : 'api/salles/'+id
+						}).then(function success(response){
+							self.salle = response.data;
+						}, function error(response){
+							
+					});
+				};
+				
+				self.save = function(){
+					if (self.salle.id == null){
+						$http({
+							method : 'POST',
+							url : 'api/salles/',
+							data : self.salle
+						}).then(function success(response){
+							self.list();
+							self.cancel();
+						}, function error (response){
+								
+						});
+					}else{
+						$http({
+							method : 'PUT',
+							url : 'api/salles/',
+							data : self.salle
+						}).then(function success(response) {
+							self.list();
+							self.cancel();
+						}, function error(response) {
+							
+						});
+					}
+				};
+					
+				self.remove = function(id) {
+					$http({
+						method : 'DELETE',
+						url : 'api/patients/'+id
+					}).then(function success(response) {
+						self.list();
+					}, function error(response) {
+						
+					});
+
+				};
+					
+				self.cancel = function() {
+					self.formation = null;
+				};
+					
+				self.list();
+		
+			},
+			controllerAs : salleCtrl
+		};
+	});
 })();
