e236d95dbf12fcb3728b40d8403be984c8abf57b
ADEVOTI <djalexisdevoti@live.fr>
2017-11-28 13:22:10 +0100

    comment medecin

--- hopital-data-angular/src/main/java/sopra/hopital/repository/MedecinRepository.java
+++ hopital-data-angular/src/main/java/sopra/hopital/repository/MedecinRepository.java
@@ -13,11 +13,11 @@ import sopra.hopital.model.MedecinSpecialite;
 
 @Transactional
 public interface MedecinRepository extends JpaRepository<Medecin, Long> {
-	@Query("select from Medecin m left join fetch MedecinSpecialite ms where m.id = :id")
-	public List<MedecinSpecialite> findAllTarifEtDuree(@Param("id") Long id);
-	
-	@Query("select from Medecin m left join fetch MedecinSpecialite ms where m.id = :idMedecin and s.id = :idSpecialite")
-	public MedecinSpecialite findTarifEtDuree(@Param("idMedecin") Long idMedecin, @Param("idSpecialite") Integer idSpecialite);
+//	@Query("select from Medecin m left join fetch MedecinSpecialite ms where m.id = :id")
+//	public List<MedecinSpecialite> findAllTarifEtDuree(@Param("id") Long id);
+//	
+//	@Query("select from Medecin m left join fetch MedecinSpecialite ms where m.id = :idMedecin and s.id = :idSpecialite")
+//	public MedecinSpecialite findTarifEtDuree(@Param("idMedecin") Long idMedecin, @Param("idSpecialite") Integer idSpecialite);
 //	
 //	@Query("select from Medecin m left join fetch Consultation c where m.id = :id")
 //	public List<Consultation> findAllConsultation(@Param("id") Long id);
