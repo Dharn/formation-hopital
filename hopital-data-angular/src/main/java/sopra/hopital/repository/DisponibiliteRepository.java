bbd5190a1957c329243da01c16c04b32c1391f56
Nicolas Rongieras <nrongier@ensc.fr>
2017-11-28 12:47:35 +0100

    maj disponibiliteRepository custom querry

--- hopital-data-angular/src/main/java/sopra/hopital/repository/DisponibiliteRepository.java
+++ hopital-data-angular/src/main/java/sopra/hopital/repository/DisponibiliteRepository.java
@@ -1,10 +1,14 @@
 package sopra.hopital.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import sopra.hopital.model.Disponibilite;
 @Transactional
 public interface DisponibiliteRepository extends JpaRepository<Disponibilite, Long> {
-
+	@Query("select d from Disponibilite d left join fetch d.medecin m where m.id = :id")
+	List<Disponibilite> listDispoByMedecin(@Param("id") Long id);
 }
