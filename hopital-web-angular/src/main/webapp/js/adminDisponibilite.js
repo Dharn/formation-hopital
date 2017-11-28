da278562fe9c6c46366246855ca524a0389f7413
MaximeDeLaMoustache <34029688+MaximeDeLaMoustache@users.noreply.github.com>
2017-11-28 11:33:15 +0100

    ajout adminDispo
    az

---
+++ hopital-web-angular/src/main/webapp/js/adminDisponibilite.js
@@ -0,0 +1,115 @@
+(function() {
+	var app = angular.module("disponibilite", []);
+
+	app
+			.directive(
+					"disponibilite",
+					function() {
+						return {
+							restrict : 'E',
+							templateUrl : "disponibilite.html",
+							controller : function($http) {
+								var self = this;
+								self.disponibilites = [];
+								self.disponibilite = null;
+
+								self.ajout = true;
+
+								self.medecins = [];
+
+								self.list = function() {
+									$http({
+										method : 'GET',
+										url : 'api/disponibilites'
+									}).then(function success(response) {
+										self.disponibilites = response.data;
+									}, function error(response) {
+
+									});
+								};
+
+								self.add = function() {
+									self.disponibiliteForm.$setPristine();
+									self.disponibilite = {};
+									self.ajout = true;
+									self.listMedecins();
+								};
+
+								self.edit = function(convention, code) {
+									self.ajout = false;
+									self.disponibiliteForm.$setPristine();
+									$http(
+											{
+												method : 'GET',
+												url : 'api/disponibilites/'
+											}).then(function success(response) {
+										self.disponibilite = response.data;
+										self.listMedecins();
+									}, function error(response) {
+
+									});
+								};
+
+								self.save = function() {
+									if (self.ajout) {
+										$http({
+											method : 'POST',
+											url : 'api/disponibilites/',
+											data : self.disponibilite
+										}).then(function success(response) {
+											self.list();
+											self.cancel();
+										}, function error(response) {
+
+										});
+									} else {
+										$http({
+											method : 'PUT',
+											url : 'api/disponibilites/',
+											data : self.disponibilite
+										}).then(function success(response) {
+											self.list();
+											self.cancel();
+										}, function error(response) {
+
+										});
+									}
+								};
+
+								self.remove = function(id) {
+									$http(
+											{
+												method : 'DELETE',
+												url : 'api/disponibilites/'
+											}).then(function success(response) {
+										self.list();
+									}, function error(response) {
+
+									});
+
+								};
+
+								self.cancel = function() {
+									self.disponibilite = null;
+								};
+
+								self.listMedecins = function() {
+									$http(
+											{
+												method : 'GET',
+												url : 'api/medecins/'
+														+ (self.disponibilite.medecin ? self.disponibilite.medecin.id
+														: '')
+											}).then(function success(response) {
+										self.medecins = response.data;
+									}, function error(response) {
+
+									});
+								};
+
+								self.list();
+							},
+							controllerAs : "disponibiliteCtrl"
+						};
+					});
+})();
