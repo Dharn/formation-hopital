abe5dc0132b074780f0586575feb0b9c01ea64e5
megCha <megard.charles@gmail.com>
2017-11-28 11:29:29 +0100

    Update

--- hopital-web-angular/src/main/webapp/js/adminDisponibilite.js
+++ hopital-web-angular/src/main/webapp/js/adminDisponibilite.js
@@ -105,7 +105,7 @@
 								};
 
 								self.list();
-								self.listTypes();
+							
 							},
 							controllerAs : "adminDisponibiliteCtrl"
 						};
--- hopital-web-angular/src/main/webapp/js/medecinDisponibilite.js
+++ hopital-web-angular/src/main/webapp/js/medecinDisponibilite.js
@@ -88,7 +88,7 @@
 								};
 
 								self.list();
-								self.listTypes();
+						
 							},
 							controllerAs : "medecinDisponibiliteCtrl"
 						};
