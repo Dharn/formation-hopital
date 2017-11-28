eeb768dca1b9a9efbf639e414f000df2527f36f4
frankybo57 <32872685+frankybo57@users.noreply.github.com>
2017-11-28 09:38:40 +0100

    Commit Medecin

--- hopital-data-angular/src/main/java/sopra/hopital/model/Medecin.java
+++ hopital-data-angular/src/main/java/sopra/hopital/model/Medecin.java
@@ -1,5 +1,53 @@
 package sopra.hopital.model;
 
-public class Medecin {
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.SequenceGenerator;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+
+@Entity
+@SequenceGenerator(name="sequenceMedecin")
+public class Medecin {
+	@Id @GeneratedValue(generator="sequenceMedecinSpecialite")
+	@Column(name="id")
+	private Integer id;
+	
+	@Version
+	@Column(name="version")
+	private int version;
+	
+	@Column(name="nom")
+	private String nom;
+	
+	@Column(name="prenom")
+	private String prenom;
+	
+	@Column(name="convention")
+	private Convention convention;
+	
+	@Column(name="carte_vitale")
+	private Boolean carteVitale;
+	
+	@Column(name="cmu")
+	private Boolean cmu;
+	
+	
+	private Secretaire secretaire;
+	
+	private Utilisateur utilisateur;
+	
+	private List<Disponibilite> listeDisponibilites;
+	
+	private List<Consultation> listeConsultations;
+	
+	private List<MedecinSpecialite>
+	
+	public Medecin(){
+		
+	}
 }
