a1fc45ba73ccac18a0f886ef9711976b678855e8
tcornilleau <34029762+tcornilleau@users.noreply.github.com>
2017-11-28 10:16:22 +0100

    repo creation

--- hopital-data-angular/src/main/java/sopra/hopital/repository/SecretaireRepository.java
+++ hopital-data-angular/src/main/java/sopra/hopital/repository/SecretaireRepository.java
@@ -3,9 +3,9 @@ package sopra.hopital.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
-import sopra.hopital.model.Medecin;
+import sopra.hopital.model.Secretaire;
 
 @Transactional
-public interface MedecinRepository extends JpaRepository<Medecin, Long> {
+public interface SecretaireRepository extends JpaRepository<Secretaire, Long>{
 
 }
