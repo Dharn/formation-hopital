6a6a2aa5eb7c16d6497b2983ba373da65e82cbfa
ADEVOTI <djalexisdevoti@live.fr>
2017-11-28 13:20:45 +0100

    comment query

--- hopital-data-angular/src/main/java/sopra/hopital/repository/MedecinRepository.java
+++ hopital-data-angular/src/main/java/sopra/hopital/repository/MedecinRepository.java
@@ -18,9 +18,9 @@ public interface MedecinRepository extends JpaRepository<Medecin, Long> {
 	
 	@Query("select from Medecin m left join fetch MedecinSpecialite ms where m.id = :idMedecin and s.id = :idSpecialite")
 	public MedecinSpecialite findTarifEtDuree(@Param("idMedecin") Long idMedecin, @Param("idSpecialite") Integer idSpecialite);
-	
-	@Query("select from Medecin m left join fetch Consultation c where m.id = :id")
-	public List<Consultation> findAllConsultation(@Param("id") Long id);
+//	
+//	@Query("select from Medecin m left join fetch Consultation c where m.id = :id")
+//	public List<Consultation> findAllConsultation(@Param("id") Long id);
 	
 //	@Query("select from Medecin m left join fetch Consultation c where m.id = :idMedecin and ")
 //	public List<Consultation> findAllConsultation(@Param("idMedecin") Long idMedecin, @Param("idSpecialite") Integer idSpecialite);
