54894c5ed62522cf4b82c787643a25b44d2ca0c4
frankybo57 <32872685+frankybo57@users.noreply.github.com>
2017-11-28 09:38:27 +0100

    Commit Specialite

--- hopital-data-angular/src/main/java/sopra/hopital/model/Specialite.java
+++ hopital-data-angular/src/main/java/sopra/hopital/model/Specialite.java
@@ -9,6 +9,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Version;
@@ -17,23 +18,29 @@ import com.fasterxml.jackson.annotation.JsonView;
 
 
 @Entity
+@SequenceGenerator(name="sequenceSpecialite")
 public class Specialite {
-	@Id @GeneratedValue
+	@Id @GeneratedValue(generator="sequenceSpecialite")
 	@Column(name="id")
 	@JsonView(Views.Common.class)
 	private Integer id;
 	
 	@Version
+	@Column(name="version")
 	private int version;
 	
 	@Column(name="intitule")
 	private String intitule;
 	
 	@OneToMany(mappedBy = "module")
-	@JsonView({Views.SpecialiteWithSalle.class})
+	//@JsonView({Views.SpecialiteWithSalle.class})
 	private List<SalleSpecialite> listeSalleSpecialite;
 	
 	@OneToMany(mappedBy = "module")
-	@JsonView({Views.SpecialiteWithMedecin.class})
+	//@JsonView({Views.SpecialiteWithMedecin.class})
 	private List<MedecinSpecialite> listeMedecinSpecialite;
+	
+	public Specialite() {
+		
+	}
 }
