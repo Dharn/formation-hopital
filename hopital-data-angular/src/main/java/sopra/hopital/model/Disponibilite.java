24f7ab39ec29753d3476eb70bd1ddf96b4b62e35
tcornilleau <34029762+tcornilleau@users.noreply.github.com>
2017-11-28 09:22:09 +0100

    bla

--- hopital-data-angular/src/main/java/sopra/hopital/model/Disponibilite.java
+++ hopital-data-angular/src/main/java/sopra/hopital/model/Disponibilite.java
@@ -13,13 +13,49 @@ import com.fasterxml.jackson.annotation.JsonView;
 public class Disponibilite {
 	
 	@Id
-	
 	@JsonView(Views.Common.class)
 	private Long id;
 	
 	@Version
-	
 	private int version;
+	
+	@JsonView(Views.Common.class)
+	private Date dtDebut;
+	
+	@JsonView(Views.Common.class)
+	private Date dtFin;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public Date getDtDebut() {
+		return dtDebut;
+	}
+
+	public void setDtDebut(Date dtDebut) {
+		this.dtDebut = dtDebut;
+	}
+
+	public Date getDtFin() {
+		return dtFin;
+	}
+
+	public void setDtFin(Date dtFin) {
+		this.dtFin = dtFin;
+	}
 	
 	
 	
--- hopital-data-angular/src/main/java/sopra/hopital/model/Secretaire.java
+++ hopital-data-angular/src/main/java/sopra/hopital/model/Secretaire.java
@@ -10,14 +10,15 @@ import com.fasterxml.jackson.annotation.JsonView;
 public class Secretaire {
 	
 	@JsonView(Views.Common.class)
-	
 	private String nom;
+	
 	@JsonView(Views.Common.class)
 	private String prenom;
 		
 	@JsonView(Views.Common.class)
 	private Utilisateur utilisateur;
 	
+	//@JsonView(Views.Medecin.class)
 	private List<Medecin> medecins;
 
 }
