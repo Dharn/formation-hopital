19bc9e0e9d8f8447c5ab2ab22f3c2ea35c15049c
gaetanlzs <34029174+gaetanlzs@users.noreply.github.com>
2017-11-28 11:48:24 +0100

    Update Patient, PatientRepo

--- hopital-data-angular/src/main/java/sopra/hopital/repository/PatientRepository.java
+++ hopital-data-angular/src/main/java/sopra/hopital/repository/PatientRepository.java
@@ -1,9 +1,14 @@
 package sopra.hopital.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import sopra.hopital.model.Patient;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
+	@Query("select p from Patient p left join fetch p.consultations c where p.id = :id")
+	Patient findOnePatientWithConsultations(@Param("id") Long id);
+	
 }
