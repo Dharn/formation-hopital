24e79ccf47e88a51e5bbf0b83cb4718c23b7cc4a
tcornilleau <34029762+tcornilleau@users.noreply.github.com>
2017-11-28 09:12:20 +0100

    creation divers

--- hopital-data-angular/src/main/java/sopra/hopital/model/Secretaire.java
+++ hopital-data-angular/src/main/java/sopra/hopital/model/Secretaire.java
@@ -1,5 +1,23 @@
 package sopra.hopital.model;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+@Entity
 public class Secretaire {
+	
+	@JsonView(Views.Common.class)
+	
+	private String nom;
+	@JsonView(Views.Common.class)
+	private String prenom;
+		
+	@JsonView(Views.Common.class)
+	private Utilisateur utilisateur;
+	
+	private List<Medecin> medecins;
 
 }
--- hopital-data-angular/src/main/java/sopra/hopital/model/Views.java
+++ hopital-data-angular/src/main/java/sopra/hopital/model/Views.java
@@ -5,5 +5,14 @@ public class Views {
 	public static class Common {
 
 	}
+	
+	public static class Secretaire {
+
+	}
+	
+	public static class Medecin {
+
+	}
+	
 
 }
